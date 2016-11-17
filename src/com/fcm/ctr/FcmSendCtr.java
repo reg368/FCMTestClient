91e18fb7a82f688d91d1ce91b27c132d0b293c18
reg368 <reg368@gmail.com>
2016-11-17 21:14:14 +0800

    fcm send ctr done

--- src/com/fcm/ctr/FcmSendCtr.java
+++ src/com/fcm/ctr/FcmSendCtr.java
@@ -1,14 +1,20 @@
 package com.fcm.ctr;
 
 import hyweb.jo.org.json.JSONObject;
-import hyweb.jo.org.json.JSONObject_old;
 import hyweb.util.PropertiesUtil;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,5 +34,35 @@ public class FcmSendCtr {
 		 JSONObject body  = new JSONObject();
 		 body.put("to", PropertiesUtil.getProp("fcm.test.token"));
 		 body.put("priority", "high");
+		 
+		 JSONObject notification   = new JSONObject();
+		 notification.put("body", "body string here");
+		 notification.put("title", "title string here");
+		 
+		 body.put("notification", notification);
+		 
+		 StringEntity entity =new StringEntity(body.toString());
+		 System.out.println("json : "+body.toString());
+		 
+		 HttpClient httpClient = HttpClientBuilder.create().build();
+		 
+		 HttpPost post = new HttpPost(PropertiesUtil.getProp("fcm.message.send.url"));
+		 post.setHeader("Authorization", "key="+PropertiesUtil.getProp("fcm.server.key"));
+		 post.setHeader("content-type", "application/json");
+		 post.setEntity(entity); 
+		 
+		 HttpResponse response = httpClient.execute(post);
+		 
+		 BufferedReader rd = new BufferedReader(
+	                new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+
+		System.out.println(result.toString());
+		 
 	 }
 }
